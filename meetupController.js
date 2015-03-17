ad86794d5003b474cbd1c25df9580c84b76105f1
stigerman <jarred.stiger@gmail.com>
2015-03-17 15:25:20 -0700

    Create meetupController.js

---
+++ meetupController.js
@@ -0,0 +1,16 @@
+var Meetup = require('../Models/Meetups.js');
+
+module.exports.create = function(req, res){
+	var meetup = new Meetup(req.body);
+	meetup.save(function(err, result){
+		res.json(result);
+
+	});
+}
+
+module.exports.list = function(req, res) {
+	Meetup.find({}, function (err, results) { 
+		res.json(results);
+
+	});
+}
